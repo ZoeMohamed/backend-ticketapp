110b43d8620087744eb6af5d394756e44adc0a67
ZoeMohamed <zoemohamed31@gmail.com>
2024-06-30 23:58:21 +0700

    crud user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,4 +14,24 @@ class UserController extends Controller
         $users = User::paginate(10);
         return view('pages.users.index', compact('users'));
     }
+
+    public function create()
+    {
+        return view('pages.users.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string',
+            'phone' => 'required',
+            'role' => 'required'
+        ]);
+
+        User::create($request->all());
+
+        return redirect()->route('users.index')->with('success', 'User Created Successuflly');
+    }
 }
